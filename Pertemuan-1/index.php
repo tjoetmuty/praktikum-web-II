af9498400ce3391cb37935a3963faf19b0016b11
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-03-26 07:29:57 +0800

    first commit

---
+++ Pertemuan-1/index.php
@@ -0,0 +1,197 @@
+<?php
+// echo "Hello World!";
+
+
+// $name = "Tjoet Muty Ahmad";
+
+// echo $name;
+// echo "<br>";
+// echo "$name";
+// echo "<br>";
+// echo "Hello My name is " . $name;
+// echo "<br>";
+// echo "This is me too $name";
+
+// //tipe data string
+// $name = "Tjoet Muty";
+// var_dump($name);
+// echo "<br>";
+
+// //tipe data integer
+// $number = 1234;
+// var_dump($number);
+// echo "<br>";
+
+// //tipe data float
+// $float = 2.4;
+// var_dump($float);
+// echo "<br>";
+
+// //tipe data boolean
+// $bool = true;
+// var_dump($bool);
+
+// //penjumlahan
+// $a = 5;
+// $b = 2;
+// $hasil = $a + $b;
+// echo "Bilangan $a + $b = " . $hasil;
+
+// //pengurangan
+// $a = 5;
+// $b = 2;
+// $hasil = $a - $b;
+// echo "Bilangan $a - $b = " . $hasil;
+
+// //perkalian
+// $a = 5;
+// $b = 2;
+// $hasil = $a * $b;
+// echo "Bilangan $a x $b = " . $hasil;
+
+// //pembagian
+// $a = 5;
+// $b = 2;
+// $hasil = $a / $b;
+// echo "Bilangan $a / $b = " . $hasil;
+
+// //modulus
+// $a = 5;
+// $b = 2;
+// $hasil = $a % $b;
+// echo "Bilangan $a % $b = " . $hasil;
+
+// //eksponen
+// $a = 5;
+// $b = 2;
+// $hasil = $a ** $b;
+// echo "Bilangan $a  <sup>$b</sup> = " . $hasil;
+
+// //assignment operator
+// $a = 5;
+// $b = 2;
+// $c = $a + $b;
+
+// echo "Bilangan a: ". $a;
+// echo "<br>";
+// echo "Bilangan b: ". $b;
+// echo "<br>";
+// echo "Bilangan c: ". $c;
+// echo "<br>";
+
+// echo "Addition Assignment: " . $a += $c;
+// echo "<br>";
+// echo "Substract Assignment: " . $a -= $c;
+// echo "<br>";
+// echo "Multiple Assignment: " . $a *= $c;
+// echo "<br>";
+// echo "Divison Assignment: " . $a /= $c;
+// echo "<br>";
+// echo "Modulus Assignment: " . $a %= $c;
+
+// //comparison operators
+// //equal
+// $a = 1;
+// $b = "1";
+
+// $result = ($a == $b) ? "true" : "false";
+// echo $result;
+
+// //identical
+// $a = 1;
+// $b = "1";
+
+// $result = ($a === $b) ? "true" : "false";
+// echo $result;
+
+// //not equal
+// $a = 1;
+// $b = "1";
+
+// $result = ($a != $b) ? "true" : "false";
+// echo $result;
+
+// //not identical
+// $a = 1;
+// $b = "1";
+
+// $result = ($a !== $b) ? "true" : "false";
+// echo $result;
+
+// //greater than
+// $a = 1;
+// $b = 10;
+
+// $result = ($a > $b) ? "true" : "false";
+// echo $result;
+
+// //less than
+// $a = 1;
+// $b = 10;
+
+// $result = ($a < $b) ? "true" : "false";
+// echo $result;
+
+// //greater than equal to
+// $a = 10;
+// $b = 10;
+
+// $result = ($a >= $b) ? "true" : "false";
+// echo $result;
+
+// //less than equal to
+// $a = 10;
+// $b = 10;
+
+// $result = ($a <= $b) ? "true" : "false";
+// echo $result;
+
+// //inc/dec operations
+// //pre ++
+// $a = 1;
+
+// echo ++$a;
+
+// //post ++
+// $a = 1;
+
+// echo $a++;
+
+// //pre --
+// $a = 10;
+
+// echo --$a;
+
+// //post --
+// $a = 10;
+
+// echo $a--;
+
+// //logical operator
+// //and
+// $result = (20 > 1 and 1 < 20) ? "true" : "false";
+// echo $result;
+// echo "<br>";
+// $result = (20 > 1 && 1 < 20) ? "true" : "false";
+// echo $result;
+
+// //or
+// $result = (20 > 1 or 1 < 20) ? "true" : "false";
+// echo $result;
+// echo "<br>";
+// $result = (20 > 1 || 1 < 20) ? "true" : "false";
+// echo $result;
+
+// //xor
+// $result = (1 >= 1 xor 10 > 1) ? "true" : "false";
+// echo $result;
+
+// //not
+// $x = true;
+// $result = (!$x) ? "true" : "false";
+// echo $result;
+
+// //php running
+echo "<h1>Hello World!</h1>";
+echo "<p>Nama saya Tjoet Muty Ahmad dan saya sedang mempelajari 
+pemrograman PHP</p>";
---
+++ Pertemuan-1/tugas-1/index.php
@@ -0,0 +1,20 @@
+<?php
+//no.1
+
+// $name = "Tjoet Muty Ahmad";
+// $age = 19;
+
+// echo "Hello, selamat datang " . $name;
+// echo "<br>";
+// echo "Umur saya $age tahun";
+
+
+
+//no.2
+$a = 15;
+$b = 17;
+$c = 2;
+$d = 5;
+$result = $a * $b / $c * $a * $d / $a;
+
+echo "$a x $b / $c x $a x $d / $a = " . $result;
